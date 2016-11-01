d7cceca238122b68f098e220a8170ea5068d117e
jonathandavidblack <jonnybl915@gmail.com>
2016-11-01 09:53:55 -0400

    added elements to page with ReactDOM object.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,11 @@
-const Backbone = require('Backbone')
+const React = require('react');
+const ReactDOM = require('react-dom')
+console.log(React);
 
-console.log(Backbone)
-document.querySelector('#app-container').innerHTML = `<h1>YOLO</h1>`
+//when we're writing our React code, we're not actually touch the DOM. We are working with the
+//virtual DOM which decides how and when to update the actual DOM. So with React we need to
+//include  a library called React-Dom which will tell us where our React code connects to the
+//actual DOM
+
+//takes as arguments the element to be rendered and then the DOM element which it is appended to
+ReactDOM.render(<h1>YADA</h1>, document.querySelector('#app-container'));
